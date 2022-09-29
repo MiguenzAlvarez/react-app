a9b3134e569afd4091f51b3e0fd1c4931340f54a
Miguenz Alvarez Ludmila <luly_mi@hotmail.com>
2022-09-29 15:15:08 -0300

    let, var, const

---
+++ src/index.js
@@ -0,0 +1,23 @@
+/*LET, VAR , CONST:
+//let -> only accessible in the block (use only when you need to reassign a variable)
+function sayHello() {
+  for (let i = 0; i < 5; i++) {
+    console.log(i);
+  }
+}
+
+sayHello();
+
+//var -> in the function 
+function sayHello() {
+  for (var i = 0; i < 5; i++) {
+    console.log(i);
+  }
+}
+
+sayHello();
+
+//const -> only accessible in the block (cant be changed)
+const x = 1;*/
+
+/*OBJECTS:*/
